e10c083c0d1ae28f8185dc3cf6213965756e5e87
jabiralawfaa <162452844+jabiralawfaa@users.noreply.github.com>
2024-10-22 11:04:01 +0700

    Update

---
+++ js/Favourite.js
@@ -0,0 +1,30 @@
+let songs = [
+  {
+    name: "Bebas",
+    artist: "HoloLive ID",
+    img: "hololive",
+    coverImg: "hololive",
+    audio: "Bebas",
+  },
+  {
+    name: "Terhebat",
+    artist: "HoloLive ID",
+    img: "hololive",
+    coverImg: "hololive",
+    audio: "Terhebat",
+  },
+  {
+    name: "Oh Asmara",
+    artist: "Kobo Kanaeru",
+    img: "hololive",
+    coverImg: "hololive",
+    audio: "OhAsmara",
+  },
+  {
+    name: "BENDERA",
+    artist: "By Maha5",
+    img: "maha5",
+    coverImg: "maha5",
+    audio: "BENDERA",
+  },
+];
